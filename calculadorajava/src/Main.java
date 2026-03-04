e1a10d994bcb98e9032dd78e7f3ab066e1fccf6d
Henriquecassol <168142302+henriquecassol09@users.noreply.github.com>
2026-03-04 07:50:11 -0300

    Add files via upload

---
+++ calculadorajava/src/Main.java
@@ -0,0 +1,15 @@
+        void main(String[]args){
+        float var1 = 2;
+        float var2 = 3;
+        float var3 = var1 + var2;
+        float var4 = var1 - var2;
+        float var5 = var1 * var2;
+        float var6 = var1 / var2;
+
+        System.out.println("a soma dos numeros 2 e 3 é " + var3);
+        System.out.println("a subtração dos numeros 2 e 3 é " + var4);
+        System.out.println("a multiplicação dos numeros 2 e 3 é " + var5);
+        System.out.println("a divisão dos numeros 2 e 3 é " + var6 );
+    };
+
+

7a43a76b4f4319d2bb682d2e69f32bd277a6b1e3
Henriquecassol09 <168142302+henriquecassol09@users.noreply.github.com>
2026-04-22 11:31:31 -0300

    Add files via upload

--- bombaDeCombustivel/src/bombaCombustivel.java
+++ bombaDeCombustivel/src/bombaCombustivel.java
@@ -1,12 +1,12 @@
 public class bombaCombustivel {
     private  String tipoCombustivel;
     private double valorLitro;
-    private double quantidadeCombustivel
+    private double quantidadeCombustivel;
 
     public bombaCombustivel(String tipoCombustivel, double valorLitro, double quantidadeCombustivel){
         this.tipoCombustivel  = tipoCombustivel;
         this.valorLitro  = valorLitro;
-        this.quantidadeCombustivel  = quantidadeCombustivel
+        this.quantidadeCombustivel  = quantidadeCombustivel;
     }
 
     public void abastecerPorValor (double valor){
@@ -27,6 +27,25 @@ public class bombaCombustivel {
         } else {
             System.out.println("erro: não a combustivel suficiente na bomba");
         }
-
+    }
+    public void alterarValor(double novoValor) {
+        this.valorLitro = novoValor;
+        System.out.println("valor do litro atualizado para "+novoValor+" R$");
+    }
+    public void alterarCombstivel(String novoCombustivel){
+        this.tipoCombustivel = novoCombustivel;
+        System.out.println("tipo de combustivel alterado para "+ novoCombustivel);
+    }
+    public void alterarQuantidadeCombustivel(double novaQuantidade){
+        this.quantidadeCombustivel = novaQuantidade;
+        System.out.println("quantidade da bomba atualizada para: "+ novaQuantidade +" litros");
+    }
+    public void exibirStatus(){
+        System.out.println("-----------------------");
+        System.out.println("combustivel " + tipoCombustivel);
+        System.out.println("valor/litro " + valorLitro + "R$");
+        System.out.println("quantidade da bomba: " + quantidadeCombustivel + " L");
+        System.out.println("-----------------------");
     }
 }
+

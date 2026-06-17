c5df51b1ac60846da2241feb0f98dd2d1dae570d
Henriquecassol09 <168142302+henriquecassol09@users.noreply.github.com>
2026-06-17 13:57:28 -0300

    Add files via upload

--- Funcionarios/src/dominio/Funcionario.java
+++ Funcionarios/src/dominio/Funcionario.java
@@ -1,30 +1,56 @@
 package dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salario;
+    private String nome;
+    private int idade;
+    private double[] salario;
+    private double media;
 
     public void imprime() {
         System.out.println(this.nome);
         System.out.println(this.idade);
         if (salario != null){
-        for (double salarios: this.salario) {
-            System.out.println(salarios + " ");
+            for (double salarios: this.salario) {
+                System.out.println(salarios + " ");
+            }
         }
     }
-}
 
     public void mediaSalarial(){
-        double media = 0;
         if(salario != null){
-        for (double salarios : salario){
-            media += salarios;
-        }
-        media /= salario.length;
-        System.out.println("sua media salarial é de "+media);
+            this.media = 0; // Limpa o valor anterior para não acumular errado
+            for (double salarios : salario){
+                media += salarios;
+            }
+            media /= salario.length;
         }
     }
 
-}
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double[] salario) {
+        this.salario = salario;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+}
--- Funcionarios/src/teste/imprimirDados.java
+++ Funcionarios/src/teste/imprimirDados.java
@@ -2,19 +2,18 @@ package teste;
 
 import dominio.Funcionario;
 
-
 public class imprimirDados {
     public static void main(String[] args) {
 
         Funcionario funcionario1 = new Funcionario();
 
-        funcionario1.nome = "henrique";
-        funcionario1.idade = 15;
-        funcionario1.salario = new double[]{1200, 2200, 1500};
-
+        funcionario1.setNome("henrique");
+        funcionario1.setIdade(15);
+        funcionario1.setSalario(new double[]{1200, 2200, 1500});
 
         funcionario1.imprime();
         funcionario1.mediaSalarial();
 
+        System.out.println("sua Média salarial é " + funcionario1.getMedia());
     }
 }
